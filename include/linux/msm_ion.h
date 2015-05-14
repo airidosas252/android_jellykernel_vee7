248ec43b7208d2982a1cd817a12b166e7567de8f
Airidas Lideikis <airidaslideikis@gmail.com>
2015-05-14 22:46:29 +0300

    gpu: ion: Add dummy msm_ion header file
    The msm specific parts of ion.h are being moved into a new header
    file. For now, add a dummy header file to get clients transitioned
    over.
    
    Change-Id: Iac039678e1c3f15d87e67aa667f7e9883ca16669
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- include/linux/msm_ion.h
+++ include/linux/msm_ion.h
@@ -1,6 +1,9 @@
 /*
  *
  * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * include/linux/ion.h
+ *
+ * Copyright (c) 2012, Code Aurora Forum. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
